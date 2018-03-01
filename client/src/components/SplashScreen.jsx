74cdee46665f775b249835af56723e4b7292825a
Alex Melagrano <alexmelagrano@gmail.com>
2018-02-28 22:24:51 -0500

    AM - clean up splashpage HTML layout

--- client/src/components/SplashScreen.jsx
+++ client/src/components/SplashScreen.jsx
@@ -43,27 +43,43 @@ export default class SplashScreen extends Component {
 
   render() {
     return (
-      <div className="SplashScreen">
-        <h1>YouMix</h1>
-        <form onSubmit={this.joinRoom}>
-          <input
-            id="room"
-            type="text"
-            placeholder="Room ID"
-            ref={(input) => this.input = input}/>
-          <button
-            type="submit"
-            className="join btn btn-primary">
-              Join
-          </button>
-        </form>
-        <br/>
-        <button
-          className="host btn btn-primary"
-          onClick={this.hostRoom}>
-            Host
-        </button>
-      </div>
+        <div className="SplashScreen">
+          <div class="container">
+            <div class="pt-5 pb-5">
+            </div>
+            <div class="row justify-content-center pt-5 pb-5">
+              <h1>YouMix</h1>
+            </div>
+            <div class="pt-5 pb-5">
+            </div>
+            <div class="row align-items-end pt-5">
+              <div class="col-4 offset-2">
+                <form onSubmit={this.joinRoom}>
+                  <div class="pb-2">
+                    <span class="pr-2">Room code:</span>
+                    <input
+                        id="room"
+                        type="text"
+                        placeholder="Room ID"
+                        ref={(input) => this.input = input}/>
+                  </div>
+                  <button
+                      type="submit"
+                      className="join btn btn-primary px-5 py-3">
+                    Join Room
+                  </button>
+                </form>
+              </div>
+              <div class="col-4 offset-2">
+                <button
+                    className="host btn btn-primary px-5 py-3"
+                    onClick={this.hostRoom}>
+                  Host Room
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
     );
   }
 }
