f569521b141ec3df2d563fd273eb72f37a97ed82
Alex Melagrano <alexmelagrano@gmail.com>
2018-03-01 16:47:04 -0500

    AM - update search results template

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -21,22 +21,26 @@ export default class SearchResults extends Component {
   render() {
     return (
       <div className="SearchResults">
-        <h2>Search Results</h2>
-        {this.props.results.map((result) => {
-          let s = result.snippet;
-          let id = result.id.videoId;
-          let thumbnail = s.thumbnails.default.url;
-          let title = s.title;
-          return (
-            <div key={id}>
-              <img src={thumbnail} />
-              {title}
-              <button onClick={() => { this.addSong({ id: id, title: title, thumbnail: thumbnail }) }}>
-                Suggest to Queue
-              </button>
-            </div>
-          );
-        })}
+          <h2>Search Results</h2>
+          <div className="row flex-wrap justify-content-between">
+              {this.props.results.map((result) => {
+                  let s = result.snippet;
+                  let id = result.id.videoId;
+                  let thumbnail = s.thumbnails.default.url;
+                  let title = s.title;
+                  return (
+                      <div key={id} className="col-3">
+                          <div className="row justify-content-center flex-column p-2 pb-3">
+                              <img src={thumbnail} className="pb-2"/>
+                              <span className="pb-2">{title}</span>
+                              <button onClick={() => { this.addSong({ id: id, title: title, thumbnail: thumbnail }) }}>
+                                  Suggest to Queue
+                              </button>
+                          </div>
+                      </div>
+                  );
+              })}
+          </div>
       </div>
     );
   }
