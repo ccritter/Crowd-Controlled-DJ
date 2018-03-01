c3c9dbc0ebc3e500a1d4906c2ccae4826a345efd
Alex Melagrano <alexmelagrano@gmail.com>
2018-03-01 10:59:49 -0500

    AM - update VoteScreen template

--- client/src/components/VoteScreen.jsx
+++ client/src/components/VoteScreen.jsx
@@ -10,9 +10,25 @@ export default class VoteScreen extends Component {
 
   render() {
     return (
-      <div className="VoteScreen">
-        <h2>Up Next</h2>
-        <SearchBar sendResults={this.props.sendResults}/>
+      <div className="VoteScreen border-right border-dark">
+          <div className="border-bottom border-dark p-3">
+              <div className="row">
+                  <div className="col-7">
+                      <h2>Up Next</h2>
+                  </div>
+                  <div className="col-5">
+                      <button type="button" className="btn btn-secondary mr-2" aria-label="Edit">
+                          Edit
+                      </button>
+                      <button type="button" className="btn btn-secondary" aria-label="Auto">
+                          Auto
+                      </button>
+                  </div>
+              </div>
+              <div className="row pl-3">
+                  <SearchBar sendResults={this.props.sendResults}/>
+              </div>
+          </div>
         <Queue socket={this.props.socket} songs={this.props.songlist}/>
       </div>
     );
