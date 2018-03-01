addbafc95d7ba279d2d6dd95575a6f3ec8db40ab
Alex Melagrano <alexmelagrano@gmail.com>
2018-03-01 18:01:17 -0500

    AM - update topbar template

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -9,9 +9,25 @@ export default class Topbar extends Component {
   render() {
     return (
       <div className="Topbar navbar navbar-inverse">
-        {/* <img src={logo} className="App-logo" alt="logo" />*/}
-        <h1 className="App-title">YouMix</h1>
-        <p>Room: {this.props.room}</p>
+          <div className="w-100 d-flex align-items-center">
+              <div className="col-1">
+                  <button type="button" onClick="">
+                      <i className="fa fa-pause"></i>
+                      <i className="fa fa-play d-none"></i>
+                  </button>
+              </div>
+              <div className="col-3">
+                  {/* master volume slider goes here */}
+              </div>
+              <div className="col-4">
+                  {/* <img src={logo} className="App-logo" alt="logo" />*/}
+                  <h1 className="App-title text-center">YouMix</h1>
+              </div>
+              <div className="col-2 offset-2">
+                  <p className="mb-1">Room code:</p>
+                  <p className="mb-0">{this.props.room}</p>
+              </div>
+          </div>
       </div>
     );
   }
